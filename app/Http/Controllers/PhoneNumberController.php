e5012a9d203653148c80511735a6bccb01152158
brianokinyi <brianokinyi.bo@gmail.com>
2018-09-11 13:13:58 +0300

    Sample working

--- app/Http/Controllers/PhoneNumberController.php
+++ app/Http/Controllers/PhoneNumberController.php
@@ -6,5 +6,13 @@ use Illuminate\Http\Request;
 
 class PhoneNumberController extends Controller
 {
-    //
+    /**
+     * Return home page.
+     *
+     * @return void
+     */
+    public function index()
+    {
+        return view('verify_phoneNumber.index');
+    }
 }
---
+++ resources/views/verify_phoneNumber/index.blade.php
@@ -0,0 +1,9 @@
+@extends('layouts.app')
+
+@section('content')
+<div>
+    <h2>
+        App works
+    </h2>
+</div>
+@endsection
